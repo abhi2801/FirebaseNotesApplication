eb6757a7427454d72318c444ca5941e436dca941
abhi2801 <abhishek.nayak560@gmail.com>
2023-08-28 10:12:33 +0530

    pushing from feature/navigationComponent

---
+++ app/src/main/java/com/example/firebasenoteapp/BaseApplication.kt
@@ -0,0 +1,8 @@
+package com.example.firebasenoteapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class BaseApplication:Application() {
+}
--- app/src/main/java/com/example/firebasenoteapp/fragments/NotesDetails.kt
+++ app/src/main/java/com/example/firebasenoteapp/fragments/NotesDetails.kt
@@ -6,7 +6,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.firebasenoteapp.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class NotesDetails : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
